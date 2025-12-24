7985b79dbbfbcf77923e0d3d98a6ab3e241670e1
Ermiyas Werdofa <ermibkl@gmail.com>
2025-12-23 22:51:51 -0500

    .eny added

--- src/Utility/firebase.jsx
+++ src/Utility/firebase.jsx
@@ -5,13 +5,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9NrooQNkfqNLTsTaGvRB0x92UKTDuYu8",
-  authDomain: "clone-6aaaf.firebaseapp.com",
-  projectId: "clone-6aaaf",
-  storageBucket: "clone-6aaaf.appspot.com",
-  messagingSenderId: "625648944980",
-  appId: "1:625648944980:web:f37c78f14dbd0a6799aac1",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
